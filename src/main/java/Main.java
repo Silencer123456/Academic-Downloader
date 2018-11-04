369c976deb71e072574e26c290d4b9e09e6fcd9d
Stepan Baratta <barattastepan@gmail.com>
2018-11-04 10:10:55 +0100

    Improved conversion from XML to JSON and writing the output file. Parsing json and saving it to MongoDB collection

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,18 @@
-import com.mongodb.BasicDBList;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import org.apache.commons.io.FileUtils;
 import org.bson.Document;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
@@ -11,15 +20,34 @@ public class Main {
         MongoClient mongoClient = MongoClients.create();
         MongoDatabase database = mongoClient.getDatabase("local");
 
-        XmlToJsonConverter converter = new XmlToJsonConverter();
-        String json = converter.convertToJson("dblpSample.xml");
+        /*XmlToJsonConverter xmlToJsonConverter = new XmlToJsonConverter();
+        String json = xmlToJsonConverter.convert("dblpSample.xml");
+
+        try {
+            xmlToJsonConverter.save(json, "output.json");
+        } catch (FileNotFoundException e) {
+            System.err.println("Error saving file.");
+            e.printStackTrace();
+        }*/
+        List<Document> docs = new ArrayList<>();
+
+        try {
+            String json = FileUtils.readFileToString(new File("output.json"), "utf-8");
+            JSONObject dblp = new JSONObject(json);
+
+            JSONArray arr = dblp.getJSONArray("phdthesis");
+
+            for (int i = 0; i < arr.length(); i++) {
+                JSONObject record = arr.getJSONObject(i);
+
+                docs.add(Document.parse(record.toString()));
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         MongoCollection<Document> collection = database.getCollection("test");
-
-        Document doc = Document.parse(json);
-
-        collection.insertOne(doc);
-
-
+        collection.insertMany(docs);
     }
 }
--- src/main/java/XmlToJsonConverter.java
+++ src/main/java/XmlToJsonConverter.java
@@ -1,18 +1,15 @@
-import com.thoughtworks.xstream.io.HierarchicalStreamReader;
-import com.thoughtworks.xstream.io.HierarchicalStreamWriter;
-import com.thoughtworks.xstream.io.copy.HierarchicalStreamCopier;
-import com.thoughtworks.xstream.io.json.JettisonMappedXmlDriver;
-import com.thoughtworks.xstream.io.xml.*;
-import org.w3c.dom.Document;
-import org.xml.sax.SAXException;
+import org.apache.commons.io.FileUtils;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.XML;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.stream.XMLInputFactory;
-import javax.xml.stream.XMLStreamException;
-import javax.xml.stream.XMLStreamReader;
-import java.io.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class XmlToJsonConverter {
 
@@ -20,44 +17,30 @@ public class XmlToJsonConverter {
      * Converts a XML document to JSON. It supports dtd entities.
      * @param pathToXmlFile - Path to the XML file
      */
-    public String convertToJson(String pathToXmlFile) {
-        BufferedReader rd = null;
-        StringReader srd = null;
-
+    public String convert(String pathToXmlFile) {
+        String jsonString = "";
         try {
+            String xml = FileUtils.readFileToString(new File(pathToXmlFile), "utf-8");
 
-            XMLInputFactory factory = XMLInputFactory.newInstance();
-            XMLStreamReader streamReader = factory.createXMLStreamReader(
-                    new FileReader(pathToXmlFile));
+            JSONObject xmlJSONObj = XML.toJSONObject(xml);
+            jsonString = xmlJSONObj.toString(4);
 
-            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-            dbf.setXIncludeAware(true);
-            dbf.setNamespaceAware(true);
-            DocumentBuilder builder = dbf.newDocumentBuilder();
-            Document doc = builder.parse(new File(pathToXmlFile));
+            System.out.println(jsonString);
 
-            HierarchicalStreamReader sourceReader = new DomReader(doc);
-
-            StringWriter buffer = new StringWriter();
-            JettisonMappedXmlDriver jettisonDriver = new JettisonMappedXmlDriver();
-            jettisonDriver.createWriter(buffer);
-            HierarchicalStreamWriter destinationWriter = jettisonDriver.createWriter(buffer);
-
-            HierarchicalStreamCopier copier = new HierarchicalStreamCopier();
-            copier.copy(sourceReader, destinationWriter);
-
-            System.out.println(buffer.toString());
-
-            return buffer.toString().replaceAll("\"\\$\"", "-");
-
-        } catch (IOException | XMLStreamException ex) {
-            System.err.println("An IOException was caught: " + ex.getMessage());
-            ex.printStackTrace();
-        } catch (ParserConfigurationException e) {
+        } catch (IOException e) {
             e.printStackTrace();
-        } catch (SAXException e) {
+            System.err.println("There was an error reading the XML file " + pathToXmlFile + ".");
+        } catch (JSONException e) {
             e.printStackTrace();
+            System.err.println("There was an error converting the XML file " + pathToXmlFile + " to JSON.");
         }
-        return "";
+
+        return jsonString;
+    }
+
+    public void save(String json, String filepath) throws FileNotFoundException {
+        try (PrintWriter out = new PrintWriter(filepath)) {
+            out.println(json);
+        }
     }
 }
